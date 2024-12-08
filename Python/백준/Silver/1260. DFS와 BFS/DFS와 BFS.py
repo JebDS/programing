fef72458884ecf1589ebd52397629ba2c85cd5a8
JE <149142334+JebDS@users.noreply.github.com>
2024-12-08 15:50:12 +0900

    [Silver II] Title: DFS와 BFS, Time: 464 ms, Memory: 46564 KB -BaekjoonHub

---
+++ Python/백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,39 @@
+import queue
+
+def dfs(li, li2, s):
+    print(N[s], end=' ')
+    li2[s]=True
+    for i in range(n):
+        if li[s][i]==1:
+            if li2[i]==False:
+                dfs(li, li2, i)
+
+def bfs(li, li2, s):
+    q=queue.Queue()
+    q.put(s)
+    li2[s]=True
+    while not q.empty():
+        a=q.get()
+        print(N[a], end=' ')
+        for i in range(n):
+            if li[a][i]==1:
+                if li2[i]==False:
+                    q.put(i)
+                    li2[i]=True
+        
+    
+
+n, m, v = map(int, input().split())
+N=[i for i in range(1,n+1)]
+li=[[0 for i in range(n)] for j in range(n)]
+lidfs=[False for i in range(n)]
+libfs=[False for i in range(n)]
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    li[a-1][b-1] = 1
+    li[b-1][a-1] = 1
+
+dfs(li, lidfs, v-1)
+print()
+bfs(li, libfs, v-1)
