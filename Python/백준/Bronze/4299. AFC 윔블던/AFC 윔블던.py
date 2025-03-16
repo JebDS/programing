2a6e416f03de9794c6894010886aa672b57181ad
JE <149142334+JebDS@users.noreply.github.com>
2025-03-17 00:36:04 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/4299. AFC 윔블던/AFC 윔블던.py
+++ Python/백준/Bronze/4299. AFC 윔블던/AFC 윔블던.py
@@ -1,5 +1,5 @@
 a, b=map(int, input().split())
-c=(a+b)/2
+c=(a+b)//2
 d=a-c
-if c+d==a and c-d==b and d>=0 and c==int(c): print(int(c), int(d))
+if c+d==a and c-d==b and d>=0: print(c, d)
 else: print(-1)
